b2540267af66ae748d61eb3048cb00b6da86a375
sohag <rafintahsinsohag@gmail.com>
2026-08-04 19:29:57 +0600

    Configured media files

---
+++ blogs/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    category_name=models.CharField(max_length=50)
+    created_at=models.DateField(auto_now_add=True)
+    updated_at=models.DateField(auto_now=True)
+
+
+    class Meta:
+        verbose_name_plural='categories'
+
+    def __str__(self):
+        return self.category_name
