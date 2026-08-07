826aada4a90ae95bd16f1a939cb59b5ca98ed9a6
sohag <rafintahsinsohag@gmail.com>
2026-08-07 20:29:00 +0600

    Add some feature like add posts_by category and somes extra things

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-li&)h&qu*9gua9y1du74(#dg5uzga8*z!--t560fr1=ms)pmg*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
