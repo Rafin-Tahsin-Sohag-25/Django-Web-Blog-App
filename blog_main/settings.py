33485cac7449f1ae61e9df30d34a672995c369e6
sohag <rafintahsinsohag@gmail.com>
2026-08-08 19:12:40 +0600

    added link on the categories and also template inheritence addedon the base.html folder

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-li&)h&qu*9gua9y1du74(#dg5uzga8*z!--t560fr1=ms)pmg*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -62,6 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'blogs.context_processors.get_categories',
             ],
         },
     },
