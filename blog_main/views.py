8303bfd2891d64eb4e941beff49d52abd841484c
sohag <rafintahsinsohag@gmail.com>
2026-08-11 19:36:31 +0600

    Add register form and make it functinal

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from blogs.models import Category,Blog
-from linkMedia.models import About
+from linkMedia.models import About 
+from.forms import registrationForm
 def home(request):
     featured_post=Blog.objects.filter(is_featured=True,status="Published").order_by('updated_at')
     posts=Blog.objects.filter(is_featured=False,status="Published")
@@ -18,3 +19,19 @@ def home(request):
 
     }
     return render (request,'home.html',context)
+
+def register(request):
+    if request.method=='POST':
+        form=registrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+        else:
+            print(form.errors)
+    else:
+       form=registrationForm()
+    context={
+        'form':form
+    }
+    return render(request,'register.html',context)
+
