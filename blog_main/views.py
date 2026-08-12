a62bc63a9b2075c5af9dae2452c44725525de455
sohag <rafintahsinsohag@gmail.com>
2026-08-12 19:47:47 +0600

    add some featured login and logout

--- blog_main/views.py
+++ blog_main/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import redirect, render
 from blogs.models import Category,Blog
 from linkMedia.models import About 
 from.forms import registrationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+from django.contrib.auth.forms import authenticate
 def home(request):
     featured_post=Blog.objects.filter(is_featured=True,status="Published").order_by('updated_at')
     posts=Blog.objects.filter(is_featured=False,status="Published")
@@ -34,4 +37,25 @@ def register(request):
         'form':form
     }
     return render(request,'register.html',context)
+
+def login(request):
+    if request.method=='POST':
+        form=AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            user=auth.authenticate(username=username,password=password)
+            if user is not None:
+                auth.login(request,user)
+            return redirect('home')
+    form=AuthenticationForm()
+    context={
+        'form':form
+    }
+    return render(request,'login.html',context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
+
 
