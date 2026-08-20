595c37e3f159a9f183c0c23b80d4594ea6bf2f1f
sohag <rafintahsinsohag@gmail.com>
2026-08-20 22:01:09 +0600

    Added Comment Feature

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Category
-from .models import Blog
+from .models import Blog,Comment
 
 
 class blogAdmin(admin.ModelAdmin):
@@ -11,3 +11,4 @@ class blogAdmin(admin.ModelAdmin):
 
 admin.site.register(Category)
 admin.site.register(Blog,blogAdmin)
+admin.site.register(Comment)
